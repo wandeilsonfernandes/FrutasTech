5f6bf2f5dcc3eacd6bf1245a10eded2eae8f66ea
Wandeilson Fernandes <wandeilsonfernandes2019@outlook.com>
2024-01-29 17:23:48 -0400

    Alteração da logo
    A logo foi alterada
    removido imagens

--- arquivos/script/home.js
+++ arquivos/script/home.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
 const user = JSON.parse(localStorage.getItem('login_success')) || false
 if(!user){
     window.location.href = 'login.html'
